846e140047d4f2b7b24c5fd6fdd41846f45d33d6
nikitatretyak <nikitatretyakov1505@gmail.com>
2025-10-21 11:59:52 +0300

    Add files via upload

---
+++ работа 1/работа 1/FileName.cpp
@@ -0,0 +1,14 @@
+#include <iostream>  
+using namespace std;
+int main() {
+    
+    int x = 10;
+    if (x > 5) {
+        cout << "x больше 5" << endl;
+    }
+    
+    else {
+        cout << "pwckcwcw";
+    }
+    1234562345677890;//quyiwuyi
+}
